103754cfa002fc899a129cd43fe083c2302d96c8
lannerate <zhanghuizaizheli@hotmail.com>
2013-11-10 16:15:40 +0800

    update initShell

--- src/InitShell.java
+++ src/InitShell.java
@@ -1,48 +1,40 @@
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+/*** Eclipse Class Decompiler plugin, copyright (c) 2012 Chao Chen (cnfree2000@hotmail.com) ***/
+import java.io.*;
 
-public class InitShell
-{
-  public static void main(String[] paramArrayOfString)
-  {
-    String str1 = ";";
-    if ((paramArrayOfString != null) && (paramArrayOfString.length > 0))
-      str1 = paramArrayOfString[0];
-    String str2 = "";
-    if ((paramArrayOfString != null) && (paramArrayOfString.length > 1))
-      str2 = paramArrayOfString[1];
-    String str3 = "start.bat";
-    if ((paramArrayOfString != null) && (paramArrayOfString.length > 2))
-      str3 = paramArrayOfString[2];
-    StringBuffer localStringBuffer = new StringBuffer("java -Xms40m -Xmx1024m -classpath ./classes");
-    localStringBuffer.append(str1);
-    File localFile = new File("lib");
-    Object localObject;
-    if (localFile.exists())
-    {
-      localObject = localFile.listFiles();
-      for (int i = 0; i < localObject.length; i++)
-        localStringBuffer.append("./lib/").append(localObject[i].getName()).append(str1);
-    }
-    localStringBuffer.append(" ").append(str2);
-    try
-    {
-      localObject = new File(str3);
-      ((File)localObject).createNewFile();
-      FileOutputStream localFileOutputStream = new FileOutputStream((File)localObject, false);
-      localFileOutputStream.write(localStringBuffer.toString().getBytes());
-      localFileOutputStream.close();
-      System.out.print("OK!\n");
-    }
-    catch (Exception localException)
-    {
-      localException.printStackTrace();
-    }
-  }
+public class InitShell {
+
+	public static void main(String args[]) {
+		String s = ";";
+		if (args != null && args.length > 0)
+			s = args[0];
+		String s1 = "";
+		if (args != null && args.length > 1)
+			s1 = args[1];
+		String s2 = "start.bat";
+		if (args != null && args.length > 2)
+			s2 = args[2];
+		StringBuffer stringbuffer = new StringBuffer(
+				"java -Xms40m -Xmx1024m -classpath ./classes");
+		stringbuffer.append(s);
+		File file = new File("lib");
+		if (file.exists()) {
+			File afile[] = file.listFiles();
+			for (int i = 0; i < afile.length; i++)
+				stringbuffer.append("./lib/").append(afile[i].getName())
+						.append(s);
+
+		}
+		stringbuffer.append(" ").append(s1);
+		try {
+			File file1 = new File(s2);
+			file1.createNewFile();
+			FileOutputStream fileoutputstream = new FileOutputStream(file1,
+					false);
+			fileoutputstream.write(stringbuffer.toString().getBytes());
+			fileoutputstream.close();
+			System.out.print("OK!\n");
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
 }
-
-/* Location:           D:\Dev_tools\ruleEngine\rbuilder.jar
- * Qualified Name:     InitShell
- * JD-Core Version:    0.6.0
- */
